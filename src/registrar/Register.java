41d82410a399146de0eca3f841d7dcba406a17ba
Jonny Ramos <jramos@uchicago.edu>
2013-04-21 18:19:49 -0500

    Reg. system

--- src/registrar/Register.java
+++ src/registrar/Register.java
@@ -10,18 +10,19 @@ public class Register {
 	public static void main(String[] args) {
 		//Registrar newRegistrar = new Registrar();
 		int option = 0;
+		Boolean isOpened = false;
 		
 		//Registrar.getInstance();
 		System.out.println("===========================================");
 		System.out.println("Register For Classes");
 	    System.out.println("===========================================\n");
 	     
-		while (option != 2) {
+		while (option != 2 && !isOpened) {
 			System.out.println();
 	        System.out.println("What would you like to do: ");
 	        System.out.println("--------------------------");
 	        
-	        System.out.println("1) Register");
+	        System.out.println("1) Open Registrar");
 	        System.out.println("2) Exit");
 	        
 	        Scanner scn = new Scanner(System.in); 
@@ -35,8 +36,44 @@ public class Register {
 			if(option == 1){
 				System.out.println();
 				Registrar.getInstance();
+				isOpened = true;
 			}
 			else if(option == 2){
+				System.out.println("Goodbye.");
+				System.exit(1);
+			}
+			else{
+				System.err.println("Please select a valid option");
+			}
+		}
+		
+		while (option != 3 && isOpened) {
+			System.out.println();
+	        System.out.println("What would you like to do: ");
+	        System.out.println("--------------------------");
+	        
+	        System.out.println("1) Open Registrar");
+	        System.out.println("2) Register");
+	        System.out.println("3) Exit");
+	        
+	        Scanner scn = new Scanner(System.in); 
+			System.out.println("Choose a number: ");
+			try {
+				option = scn.nextInt();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+			}
+		
+			if(option == 1){
+				System.out.println();
+				Registrar.getInstance();
+				isOpened = true;
+			}
+			else if(option == 2){
+				System.out.println();
+				register();
+			}
+			else if(option == 3){
 				System.out.println("Goodbye.");
 				System.exit(1);
 			}
@@ -47,5 +84,9 @@ public class Register {
     	
 
     }
+	
+	private static void register(){
+		Registrar.registerClasses();
+	}
 
 }
--- src/registrar/Registrar.java
+++ src/registrar/Registrar.java
@@ -1,13 +1,25 @@
 package registrar;
 
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Registrar {
 	static java.util.Date now;
 	private static Registrar myRegistrar;
+	private static Hashtable<String,Integer> classes;
 	
 	
 	   private Registrar() {
-	      // Exists only to defeat instantiation.
+		   
 		   now = new java.util.Date();
+		   classes = new Hashtable<String,Integer>();
+		   
+		   classes.put("Algorithms", 3);
+		   classes.put("Object Oriented Design", 0);
+		   classes.put("Networks", 1);
+		   classes.put("Databases", 2);
+		   classes.put("iOS Development", 4);
 	   }
 	  
 	   
@@ -18,10 +30,57 @@ public class Registrar {
 	         System.out.println(now.toString());
 	      }
 	      else{
-	    	  System.err.println("Registrar Already Exists.");
+	    	  System.err.println("Registrar Already Opened.");
 	    	  System.err.println("Current Registrar: " + now.toString());
 	      }
 	     
 	      return myRegistrar;
+	   }
+	   
+	   public static void registerClasses(){
+		   
+		   String currClass = null;
+		   int option = 0;
+		   
+		   Iterator<String> iterator = classes.keySet().iterator();  
+		   Iterator<String> iterator2 = classes.keySet().iterator();  
+		   
+		   System.out.println("Choose a class to register: ");
+	        System.out.println("--------------------------");
+	        
+	        System.out.println("Class Name || Available Seats");
+	        System.out.println();
+		   
+		   int count=1;
+		   while (iterator.hasNext()) {  
+		      String key = iterator.next().toString();  
+		      String value = classes.get(key).toString();  
+		      
+		      System.out.println(count + ") " + key + " || " + value);  
+		      count++;
+		   }  
+		   Scanner scn = new Scanner(System.in); 
+		   System.out.println("Choose a number: ");
+			
+			try {
+				option = scn.nextInt();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+			}
+			count=1;
+			   while (iterator2.hasNext()) {  
+				   if(count == option){
+					   currClass = iterator2.next().toString(); 
+				   }
+				   iterator2.next();
+			      count++;
+			   }  
+			   if(classes.get(currClass) == 0){
+				   System.err.println("Student not registered. " + currClass + " is full.");
+			   }
+			   else{
+				   classes.put(currClass, classes.get(currClass) - 1);
+				   System.err.println("Student is now registered for " + currClass);
+			   }
 	   }
 	}
